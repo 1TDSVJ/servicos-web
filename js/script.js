58a69bc7517ac1f37af86b54d86079bc2c0ec0d1
LucasGuerraS <rm95084@fiap.com.br>
2022-05-04 09:41:02 -0300

    arrays e loops

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 // console.log("Mensagem que veio do arquivo externo")
 // document.write("Wscrevendo com document!")
 
+/*
 
 //Declaração de variáveis com var.
 //Basicamente o que se tem com var é o escopo aumentado.
@@ -43,3 +44,54 @@ elementoBotao.addEventListener("mouseout",function(){
     elementoBotao.style.height = "30px"
     elementoBotao.style.width = "60px"
 })
+*/
+
+//var nome = "Lucas"
+
+//Arrays - Vetores /Vetores são arrays que podem armazenar qualquer tipo de dado.
+var nomes = ["Lucas", "João", "Pedro", "Gustavo"]
+
+//Acessando um elemento do array
+console.log("Conteúdo do Array: " + nomes[0])
+
+//Tamanho do Array
+console.log("Tamanho do Array: " + nomes.length)
+
+//Adicionando elementos no final do array
+nomes.push("Alberto")
+console.log("Conteúdo do Array: " + nomes[4])
+
+//Adicionando elementos no início do array
+nomes.unshift("Ana")
+console.log("Conteúdo do Array: " + nomes[0])
+
+//Removendo elementos do final do array
+nomes.pop()
+console.log("Conteúdo do Array: " + nomes[3])
+
+//Removendo elementos do início do array
+nomes.shift()
+console.log("Conteúdo do Array: " + nomes[0])
+
+//Localizando um elemento no array
+console.log("Localização do elemento: " + nomes.indexOf("Pedro"))
+
+//Removendo elementos de um índice específico
+let posicao = nomes.indexOf("Pedro")
+nomes.splice(posicao, 1)
+console.log("Conteúdo do Array: " + nomes[1])
+
+//Iterando um Array
+for(let i = 0; i < nomes.length; i++){
+    console.log("Nome: " + i + " " + nomes[i])
+}
+
+//Loop For Of - Iterando um Array
+for(let nome of nomes){//nome é um elemento do array
+    console.log("Valor de nomes: " + nome)
+}
+
+//Loop For In - Iterando um Objeto
+for(let indice in nomes){//indice é um indice do array
+    console.log("Valor de pessoa: " + nomes[indice])
+}
