633af356b885d943456fd8f5307a411c2fc75b3e
LuisFelipeGM <rm94051@fiap.com.br>
2022-05-04 09:41:13 -0300

    Novas funções e laços de repetição no JS

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //console.log('Mensagem que veio do arquivo Externo.')
 //document.write('Escrevendo uma mensagem com document!')
 
-
+/*
 // Declaração de variáveis com var.
 // Basicamente o que se tem com var é o escopo aumentado. 
 var nome = "Alê"
@@ -20,6 +20,7 @@ sobrenome()
 
 //Constantes em JS são muito utilizadas para o armanezamento de
 //elementos, porque os elementos são imutaveis, mas os seus atributos não.
+
 const elementoBotao = document.querySelector("#botao")
 
 //Vamos trellar uma funçao de listener/escutador de eventos ao elemento
@@ -42,3 +43,72 @@ elementoBotao.addEventListener("mouseout",function(){
     elementoBotao.style.height = "50px"
     elementoBotao.style.width = "100px"
 })
+*/
+
+//ARRAYS - Vetores
+var nomes = ["Alê", "Carlos", "João", "Maria"]
+
+//Acessando um elemento do array
+console.log("Conteúdo do array : " + nomes[0])
+
+//Tamano do array
+console.log("Tamanho do array : " + nomes.lengt)
+
+//Adicionand elementos ao fianl do array
+nomes.push("Ciclano")
+console.log("Conteúdo do array : " + nomes[4])
+
+//Adicionand elementos ao inicio do array
+nomes.unshift("Luffer")
+console.log("Conteúdo do array : " + nomes[0])
+
+//Removendo elemenos do final do array
+nomes.pop()
+console.log("Conteúdo do array : " + nomes[4]) 
+
+//Removendo elemenos do inicio do array
+nomes.shift()
+console.log("Conteúdo do array : " + nomes[0])
+
+//Localizando um elemento no array
+console.log("Localizando um elemento no array: " + nomes.indexOf("Carlos"))
+
+
+//Removendo um elemento do arra com localização
+let posicao = nomes.indexOf("João")
+nomes.splice(posicao,1)
+
+
+//Iterando um array
+for(let i = 0; i < nomes.length; i++){
+    console.log("Nomes : " + nomes[i])
+}
+
+//Objeto - JSON
+var pessoa = {
+    nome : "",
+    sobrenome : "",
+    idade : "",
+}
+
+//LOOP FOR OF - Iterando um array
+for(let nome of nomes){//nome é um elemento do array
+
+    //Objeto - JSON
+    var pessoa = {
+        nome : "",
+        sobrenome : "",
+        idade : "",
+    }
+
+    pessoa.nome = nome
+
+    console.log("Valor de nomes : " + nome)
+
+}
+
+//FOR IN - Iterando um objeto
+for(let indice in nomes){//indice é um indice do array
+    console.log("Valor de pessoa : " + nomes[indice])
+}
+
