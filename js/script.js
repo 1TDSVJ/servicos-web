a98a06097896e1faf3c1a5fb8bee2a8c6970f223
heitorborbamarini <rm92976>
2022-05-04 09:44:31 -0300

    Atualizando JS

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 /*
 Comentário de Bloco
 */
-//Mensagem de console vi8nda do arquivo externo js
+//Mensagem de console vinda do arquivo externo js
 //console.log('Mensagem que veio do arquivo Externo')
 //document.write('Escrevendo uma mensagem com document!')
 
 //Declaração variavel com var
-
+/*
 var nome = "Heitor"
 console.log("Meu nome é : " + nome)
 
@@ -44,3 +44,55 @@ elementoBotao,addEventListener('mouseout', function(){
     elementoBotao.style.height = '50'
     elementoBotao.style.width = '100px'
 })
+*/
+
+//var nome = "Heitor "
+//Array - Vetores
+var nomes = ["Heitor", "Borba", "Marini"]
+
+nomes.unshift('Mateus')
+console.log("Conteudo do Array: " )
+
+//Removendo elementos final do array
+nomes.pop()
+console.log('Conteudo Array: '+ nomes[3])
+console.log("Tamanho do Array: " + nomes.length)
+
+//Remover o primeiro
+nomes.shift()
+console.log("Conteudo array: "+ nomes[1])
+
+//Localizando elementos array
+console.log('Localizando um elemento no array: '+ nomes.indexOf('Heitor'))
+
+//Remove elemento do array com localização
+let posicao = nomes.indexOf('João')
+nomes.splice(posicao,1)
+
+//Interando um Array
+for(let i = 0; i < nomes.length; i++){
+    console.log("Nome: " + nomes[i])
+}
+
+//objeto - JSON
+
+   
+
+
+//LOOP FOR OF -Interando um array
+for(let nome of nomes){//nome é um elemento do array
+   
+    var pessoa = {
+        nome: "",
+        sobrenome: "",
+        idade: "",
+    }
+    pessoa.nome = nome
+    console.log("Valor de nomes: " + nome)
+
+   
+}
+//FOR IN -INterando um objeto
+for(let indice in nomes){
+    console.log('Valor da pessoa: ' + nomes[indice])
+}
